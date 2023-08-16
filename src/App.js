6d56863662acc449fc453dfd57e20aaf2bfafd61
aman <amanmaksat0@gmail.com>
2023-08-17 01:23:10 +0600

    complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import ContributionGraph from './components/ContributionGraph/ContributionGraph';
 import './App.css';
 
-const App = () => (
-  <div className="App">
+const App = () => {
+  const [contributionData, setContributionData] = useState(null);
 
-  </div>
-);
+  useEffect(() => {
+    const getContributionData = async () => {
+      try {
+        const { data } = await axios('https://dpg.gg/test/calendar.json');
+        setContributionData(data);
+      } catch (error) {
+        console.error('Error fetching contribution data:', error);
+      }
+    };
+
+    getContributionData().catch();
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>Contribution Graph Example</h1>
+      {contributionData && <ContributionGraph data={contributionData} />}
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/ContributionGraph/ContributionGraph.js
@@ -0,0 +1,120 @@
+import React, { useState } from 'react';
+import './ContributionGraph.css';
+
+const daysOfWeek = ['Пн', 'Ср', 'Пт'];
+const months = [
+  'Апр', 'Май', 'Инюь', 'Июль', 'Авг', 'Сент',
+  'Окт', 'Нояб', 'Дек', 'Янв', 'Февр', 'Март'
+];
+
+const ContributionGraph = ({ data }) => {
+  const [activeTooltip, setActiveTooltip] = useState(null);
+
+  const formatDate = inputDate => {
+    const options = { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric' };
+    return new Date(inputDate).toLocaleDateString('ru-RU', options);
+  };
+
+  const getNumberForClass = count => {
+    if (count === 0) return 0;
+    if (count >= 1 && count <= 9) return 1;
+    if (count >= 10 && count <= 19) return 2;
+    if (count >= 20 && count <= 29) return 3;
+    return 4;
+  };
+
+  const contributionsWithNoData = [];
+  const firstDate = new Date(Object.keys(data)[0]);
+  const nextYear = firstDate.getFullYear() + 1;
+
+  const currentDate = new Date(firstDate);
+  currentDate.setDate(currentDate.getDate() - 1);
+  while (currentDate < new Date(nextYear, firstDate.getMonth(), firstDate.getDate() - 2)) {
+    const isoDate = currentDate.toISOString().split('T')[0];
+    contributionsWithNoData.push(isoDate);
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+
+  const contributionsByDate = {};
+  Object.keys(data)?.forEach(date => {
+    contributionsByDate[date] = data[date];
+  });
+
+  return (
+    <div className="contribution-graph">
+      <div className="months">
+        {months.map(month => (
+          <div key={month} className="month">{month}.</div>
+        ))}
+      </div>
+      <div className="contribution-graph-inner">
+        <div className="days-of-week">
+          {daysOfWeek.map(day => (
+            <div key={day} className="day-of-week">{day}</div>
+          ))}
+        </div>
+        <div className="grid">
+          {contributionsWithNoData.map(date => (
+            <div
+              key={date}
+              className={`contribution-box contribution-${getNumberForClass(contributionsByDate[date] || 0)} ${activeTooltip === date ? ' visible' : ''}`}
+              onClick={() => setActiveTooltip(date)}
+            >
+              <span className={`tooltip ${activeTooltip === date ? 'visible' : ''}`}>
+                <span>{contributionsByDate[date] || 'No'} contributions</span>
+                <span>{formatDate(date)}</span>
+              </span>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="contribution-graph-under">
+        <p>Меньше</p>
+        <div
+          className='contribution-box contribution-0'
+          onClick={() => setActiveTooltip('preview-1')}
+        >
+          <span className={`tooltip ${activeTooltip === 'preview-1' ? 'visible' : ''}`}>
+            No contributions
+          </span>
+        </div>
+        <div
+          className='contribution-box contribution-1'
+          onClick={() => setActiveTooltip('preview-2')}
+        >
+          <span className={`tooltip ${activeTooltip === 'preview-2' ? 'visible' : ''}`}>
+            1-9 contributions
+          </span>
+        </div>
+        <div
+          className='contribution-box contribution-2'
+          onClick={() => setActiveTooltip('preview-3')}
+        >
+          <span className={`tooltip ${activeTooltip === 'preview-3' ? 'visible' : ''}`}>
+            10-19 contributions
+          </span>
+        </div>
+        <div
+          className='contribution-box contribution-3'
+          onClick={() => setActiveTooltip('preview-4')}
+        >
+          <span className={`tooltip ${activeTooltip === 'preview-4' ? 'visible' : ''}`}>
+            20-29 contributions
+          </span>
+        </div>
+        <div
+          className='contribution-box contribution-4'
+          onClick={() => setActiveTooltip('preview-5')}
+        >
+          <span className={`tooltip ${activeTooltip === 'preview-5' ? 'visible' : ''}`}>
+            30+ contributions
+          </span>
+        </div>
+        <p>Больше</p>
+      </div>
+    </div>
+  );
+};
+
+export default ContributionGraph;
